3ae726a987b99cf5bd4f53d3e11f857ad433e294
Abraham Sesay <abrahamsesay96@gmail.com>
2020-07-26 22:53:37 -0500

    add data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const nav = [
+    "Workouts & Programs",
+    "Nutrition Plans",
+    "Community"
+]
+
+const heading = [
+    "It's Time to Regain Your Fitness"
+]
+
+const subHeading = [
+    "Do fitness anywhere and anytime with our training videos"
+]
+
+const footerCards = [
+    {
+        key: 1,
+        title: "Daily Workouts 7 Days a Week",
+        content: "Get personalised workouts on a daily basis from professionals"
+    },
+    {
+        key: 2,
+        title: "Access From Any Platform",
+        content: "Use all the benefits of the service anywhere and on any device"
+    },
+    {
+        key: 3,
+        title: "Guides & Community",
+        content: "A private community where we help each other"
+    }
+]
+
+
+export {nav, heading, subHeading, footerCards} 
